8e7a349ad955583a2a0a50430c6075f5f600a6b7
gabedvdsn <73317121+gabedvdsn@users.noreply.github.com>
2024-12-12 18:56:18 -0800

    init

--- interpreter.py
+++ interpreter.py
@@ -2,6 +2,7 @@ import sys
 from lark import Lark, Transformer, Tree
 import lark
 import os
+from colorama import Fore, Style
 
 #print(f"Python version: {sys.version}")
 #print(f"Lark version: {lark.__version__}")
@@ -17,12 +18,27 @@ def interpret(source_code):
     results = []
     for stmt in statements:
         expr = stmt[1] if stmt[0] == 'statement' else stmt
+        print(rec_print(expr, 0))
         results.append(linearize(evaluate(expr)))
         
     return " ;; ".join(str(x) for x in results)
 
 # convert concrete syntax to CST
 parser = Lark(open("grammar.lark").read(), parser='lalr')
+
+def rec_print(tree, depth):
+    tabs = depth * '  '
+    tabbed = False
+    for item in tree:
+        if isinstance(item, tuple):
+            print()
+            rec_print(item, depth + 1)
+        else:
+            if not tabbed:
+                print(f"{tabs}{item}", end=' ')
+                tabbed = True
+            else:
+                print(f"{item}", end=' ')
 
 # convert CST to AST
 class LambdaCalculusTransformer(Transformer):
@@ -35,56 +51,86 @@ class LambdaCalculusTransformer(Transformer):
         
     def lam(self, args):
         name, body = args
+        print(f"LAM-----------------\n\t{name}\n\t{body}\n\t{args}")
+        print("-----------------")
         return ('lam', str(name), body)
 
     def app(self, args):
+        print(f"APP-----------------\n\t{args[0]}\n\t{args[1]}")
+        print("-----------------")
         return ('app', *args)
 
     def var(self, args):
         token, = args
+        print(f"VAR-----------------\n\t{token}")
+        print("-----------------")
         return ('var', str(token))
 
     def NAME(self, token):
+        print(f"NAME-----------------\n\t{token}")
+        print("-----------------")
         return str(token)
 
     def number(self, args):
         token, = args
+        print(f"NUMBER-----------------\n\t{token}")
+        print("-----------------")
         return float(token)
     
     def plus(self, args):
+        print(f"PLUS-----------------\n\t{args[0]}\n\t{args[1]}")
+        print("-----------------")
         return ('plus', ('number', args[0]), ('number', args[1]))
 
     def minus(self, args):
+        print(f"MINUS-----------------\n\t{args[0]}\n\t{args[1]}")
+        print("-----------------")
         return ('minus', ('number', args[0]), ('number', args[1]))
     
     def mul(self, args):
+        print(f"MUL-----------------\n\t{args[0]}\n\t{args[1]}")
+        print("-----------------")
         return ('mul', ('number', args[0]), ('number', args[1]))
 
     def neg(self, args):
+        print(f"NEG-----------------\n\t{args[0]}\n\t{args[1]}")
+        print("-----------------")
         return ('neg', ('number', args[0]))
 
     def if_(self, args):
+        print(f"IF-----------------\n\t{args[0]}\n\t{args[1]}\n\t{args[2]}")
+        print("-----------------")
         return ('if', ('number', args[0]), ('number', args[1]), ('number', args[2]))
 
     def leq(self, args):
         # Handles: exp <= exp
+        print(f"LEQ-----------------\n\t{args[0]}\n\t{args[1]}")
+        print("-----------------")
         return ('leq', ('number', args[0]), ('number', args[1]))
     
     def eq(self, args):
         # Handles: exp == exp
+        print(f"EQ-----------------\n\t{args[0]}\n\t{args[1]}")
+        print("-----------------")
         return ('eq', ('number', args[0]), ('number', args[1]))
 
     def let(self, args):
         # Handles: let NAME = exp in exp
         name, expr1, expr2 = args
+        print(f"LET-----------------\n\t{name}\n\t{expr1}\n\t{expr2}")
+        print("-----------------")
         return ('let', (str(name)), (expr1), (expr2))
 
     def fix(self, args):
         expr, = args
+        print(f"FIX-----------------\n\t{expr}")
+        print("-----------------")
         return ('fix', expr)
     
     def rec(self, args):
         name, expr1, expr2 = args
+        print(f"REC-----------------\n\t{name}\n\t{expr1}\n\t{expr2}")
+        print("-----------------")
         return ('let', (str(name)), ('fix',expr1), (expr2))
 
     def statement(self, args):
@@ -108,23 +154,29 @@ class LambdaCalculusTransformer(Transformer):
 
 # reduce AST to normal form
 def evaluate(tree):
-    print(tree)
+    print(f"TOP OF EVAL", tree)
     if isinstance(tree, (int, float, str)):
         result = tree
     elif tree[0] == 'app':
-        print("TREE " + str(tree))
-        e1 = evaluate(tree[1])
-
-        if e1[0] == 'lam':
-            body = e1[2]
-            name = e1[1]
-            arg = tree[2]
-            rhs = substitute(body, name, arg)
-            result = evaluate(rhs)
-            pass
+        if tree[1][0] == 'fix':
+            _tree = ('fix', tree[2], tree[1][1])
+            result = evaluate(_tree)
         else:
-            result = ('app', e1, tree[2])
-            pass
+            e1 = evaluate(tree[1])
+
+            if e1[0] == 'lam':
+                body = e1[2]
+                name = e1[1]
+                arg = tree[2]
+                rhs = substitute(body, name, arg)
+                result = evaluate(rhs)
+                pass
+            elif e1[0] == 'var':
+                print("FOUND VARRRR")
+                result = evaluate(tree[2])
+            else:
+                result = ('app', e1, tree[2])
+                pass
     elif tree[0] == 'plus':
         result = evaluate(tree[1]) + evaluate(tree[2])
     elif tree[0] == 'minus':
@@ -144,6 +196,7 @@ def evaluate(tree):
         _else = evaluate(tree[3])
 
         if _if == 0.0:  # False
+            print(f"\tFALSE: {_else} for {tree[3]}")
             result = ('var', _else)
         else:  # true
             result = ('var', _then)
@@ -157,6 +210,22 @@ def evaluate(tree):
 
     elif tree[0] == 'let':
         result = evaluate(substitute(tree[3], tree[1], (tree[2])))
+    elif tree[0] == 'fix':
+        print("FIXED")
+        print(tree)
+        _if = evaluate(substitute(tree[2][2], 'n', tree[1]))
+        print("IF" + str(_if))
+        _tree = substitute(tree[2], 'n', ('number', tree[1]))
+        print(f"TREE: {_tree}")
+        print(tree[1][2])
+        print(tree[1][2][1])
+        cond = tree[1][2][1]
+        result = tree
+        
+        # if tree[2][0] == 'fix':
+        #     result = letrec_evaluate(tree[1], tree[2], tree[3][2])
+        # else:
+        #     ...
 
     # elif tree[0] == 'rec':
     #     result = evaluate(substitute(tree[3], tree[1], (tree[2])))
@@ -190,6 +259,7 @@ def evaluate(tree):
         pass
     return result
 
+
 # generate a fresh name 
 # needed eg for \y.x [y/x] --> \z.y where z is a fresh name)
 class NameGenerator:
@@ -210,48 +280,48 @@ def substitute(tree, name, replacement):
     print(tree)
     # tree [replacement/name] = tree with all instances of 'name' replaced by 'replacement'
     if(isinstance(tree, float)):
-        return tree
+        result = tree
     elif tree[0] == 'var':
         if tree[1] == name:
             print("GTTEM")
-            return replacement # n [r/n] --> r
+            result = replacement # n [r/n] --> r
         else:
-            return tree # x [r/n] --> x
+            result = tree # x [r/n] --> x
     elif tree[0] == 'lam':
         if tree[1] == name:
-            return tree # \n.e [r/n] --> \n.e
+            result = tree # \n.e [r/n] --> \n.e
         else:
             fresh_name = name_generator.generate()
-            return ('lam', fresh_name, substitute(substitute(tree[2], tree[1], ('var', fresh_name)), name, replacement))
+            result = ('lam', fresh_name, substitute(substitute(tree[2], tree[1], ('var', fresh_name)), name, replacement))
             # \x.e [r/n] --> (\fresh.(e[fresh/x])) [r/n]
     elif tree[0] == 'app':
-        return ('app', substitute(tree[1], name, replacement), substitute(tree[2], name, replacement))
+        result = ('app', substitute(tree[1], name, replacement), substitute(tree[2], name, replacement))
 
     elif tree[0] == 'plus':
-        return ('plus', substitute(tree[1], name, replacement), substitute(tree[2], name, replacement))
+        result = ('plus', substitute(tree[1], name, replacement), substitute(tree[2], name, replacement))
     elif tree[0] == 'minus':
-        return ('minus', substitute(tree[1], name, replacement), substitute(tree[2], name, replacement))
+        result = ('minus', substitute(tree[1], name, replacement), substitute(tree[2], name, replacement))
     elif tree[0] == 'mul':
-        return ('mul', substitute(tree[1], name, replacement), substitute(tree[2], name, replacement))
+        result = ('mul', substitute(tree[1], name, replacement), substitute(tree[2], name, replacement))
 
     elif tree[0] == 'number':
         if isinstance(tree[1], (float, int)):
-            return ('number', tree[1])
+            result = ('number', tree[1])
         else:
-            return ('number', substitute(tree[1], name, replacement))
+            result = ('number', substitute(tree[1], name, replacement))
     elif tree[0] == 'neg':
-        return tree
+        result = tree
 
 
     elif tree[0] == 'if':
         print(substitute(tree[1], name, replacement))
-        return ('if', substitute(tree[1], name, replacement), substitute(tree[2], name, replacement), substitute(tree[3], name, replacement))
+        result = ('if', substitute(tree[1], name, replacement), substitute(tree[2], name, replacement), substitute(tree[3], name, replacement))
 
     elif tree[0] == 'eq':
-        return ('eq', substitute(tree[1], name, replacement), substitute(tree[2], name, replacement))
+        result = ('eq', substitute(tree[1], name, replacement), substitute(tree[2], name, replacement))
 
     elif tree[0] == 'leq':
-        return ('leq', substitute(tree[1], name, replacement), substitute(tree[2], name, replacement))
+        result = ('leq', substitute(tree[1], name, replacement), substitute(tree[2], name, replacement))
 
     elif tree[0] == 'let':
         print('\t' + str(tree))
@@ -260,8 +330,9 @@ def substitute(tree, name, replacement):
             tree = evaluate(tree)
         
         if isinstance(tree, (int, float)):
-            return ('number',tree)
-        return ('let', tree[1], substitute(tree[2], name, replacement), substitute(tree[3], name, replacement))
+            result = ('number',tree)
+        else:
+            result = ('let', tree[1], substitute(tree[2], name, replacement), substitute(tree[3], name, replacement))
     elif tree[0] == 'rec':
         print('\t' + str(tree))
 
@@ -269,22 +340,26 @@ def substitute(tree, name, replacement):
             tree = evaluate(tree)
         
         if isinstance(tree, (int, float)):
-            return ('number',tree)
-        return ('rec', tree[1], substitute(tree[2], name, replacement), substitute(tree[3], name, replacement))
+            result = ('number',tree)
+        else:
+            result = ('rec', tree[1], substitute(tree[2], name, replacement), substitute(tree[3], name, replacement))
     elif tree[0] == 'fix':
-        return ('fix', substitute(tree[1],name,replacement))
+        result = ('fix', substitute(tree[1],name,replacement))
 
     elif tree[0] == 'cons':
-        return ('cons', substitute(tree[1], name, replacement), substitute(tree[2], name, replacement))
+        result = ('cons', substitute(tree[1], name, replacement), substitute(tree[2], name, replacement))
     elif tree[0] == 'hd':
-        return ('hd', substitute(tree[1], name, replacement))
+        result = ('hd', substitute(tree[1], name, replacement))
     elif tree[0] == 'tl':
-        return ('tl', substitute(tree[1], name, replacement))
+        result = ('tl', substitute(tree[1], name, replacement))
     elif tree[0] == 'nil':
-        return tree
+        result = tree
 
     else:
         raise Exception('Unknown tree', tree)
+    
+    print(f"SUB RES: {result}")
+    return result
     
 
 def linearize(ast):
